207353b4974b9658583d4d1d5c7df6b3b86c6ad5
Vishwas Acharya <vishwasracharya@gmail.com>
2021-12-27 21:03:39 +0530

    Day-1

---
+++ Javascript/Data-Structure/SinglyLinkedList.js
@@ -0,0 +1,22 @@
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+        return this;
+    }
+}
+
+let list = new SinglyLinkedList();
+list.push('Hii');

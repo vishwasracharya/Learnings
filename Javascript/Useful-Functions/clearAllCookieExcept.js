7b359863f86bd16f9022f0332e79b95d9349c7f0
Vishwas Acharya <vishwasracharya@gmail.com>
2022-07-12 17:11:30 +0530

    Create clearAllCookieExcept.js

---
+++ Javascript/Useful-Functions/clearAllCookieExcept.js
@@ -0,0 +1,9 @@
+var cookies = document.cookie.split(';');
+for (var i = 0; i < cookies.length; i++) {
+  var cookie = cookies[i];
+  var eqPos = cookie.indexOf('=');
+  var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+  if (name != 'NameOfTheCookie' && name != 'NameOfTheCookie') {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+  }
+}
